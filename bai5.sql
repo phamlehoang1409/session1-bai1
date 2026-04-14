a12bdf3883e1c54e55aa4d095a600d23d16e065c
phamlehoang1409 <phamhoangnee1@gmail.com>
2026-04-14 18:33:42 +0700

    bai.sql

---
+++ bai5.sql
@@ -0,0 +1,11 @@
+/*
+Kịch bản rủi ro
+Nếu không có các ràng buộc chặt chẽ, hệ thống sẽ đối mặt với các lỗi nghiêm trọng sau:
+
+ 1 Số dư âm
+
+ 2 Trùng lặp ví
+
+ 3 Số tiền giao dịch bị nhập số âm
+*/
+
